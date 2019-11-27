3c55e8b3c72b1ab311b636865a7ed4b9f109178f
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-11-27 18:12:15 +0100

    som problem

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,27 +1,57 @@
 import React,{useState,useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input'
 import './Chat.css';
 let socket;
 const Chat = ({location})=> {
     const [name,setName] = useState('');
     const [room,setRoom] = useState('');
+    const [message,setMessage] = useState('');
+    const [messages,setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
     useEffect(() => {
         const {name,room} = queryString.parse(location.search);
         socket = io(ENDPOINT);
-        setName(name);
         setRoom(room);
-        socket.emit('join',{name,room},() =>{
-
+        setName(name);
+        
+        socket.emit('join',{name,room},(error) =>{
+            if(error){
+                alert(error)
+            }
         });
         return () => {
             socket.emit('disconnect');
             socket.off();
         }
-    },[ENDPOINT,Location.search])
+    },[ENDPOINT,Location.search]);
+    useEffect(() => {
+        socket.on('message',(message) =>{
+            setMessages([...messages,message]);
+        })
+    },[messages]);
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message){
+
+            socket.emit('sendMessage',message,() => setMessage(''));
+        }
+    }
+    console.log(message,messages);
+    
     return (
-        <h1>Chat</h1>
+        <div className = "outerContainer">
+            <div className = "container">
+                <InfoBar room = {room}/>
+                <Input 
+                message = {message} 
+                setMessage = {setMessage} 
+                sendMessage = {sendMessage}
+                />
+            </div>
+        </div>
     )
 };
 export default Chat;
--- server/USERS.js
+++ server/USERS.js
@@ -9,13 +9,14 @@ if(existingUser){
 const user = {id,name,room};
 users.push(user)
 return {user}
-}
-const removeUser = () => {
-
-}
-const getUser = () => {
-
-}
-const getUsersInRoom = () => {
-
-}
+} ///////////////////
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id ===id)
+    if(index !== -1) return users.splice(index,1)[0];
+    
+} ///////////////////
+const getUser = (id) => users.find((user) => user.id === id )
+ ///////////////////
+const getUsersInRoom = (room) =>  users.filter((user) => user.room === room);
+ ///////////////////
+module.exports = {addUser,removeUser,getUser,getUsersInRoom};
