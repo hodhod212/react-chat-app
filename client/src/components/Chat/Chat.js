cff94028f25007d5508284e0ea874500f725ea8e
Samer Senbol <samer.senbol@gmail.com>
2019-12-02 11:43:52 +0100

    Adding the / messasge to the chat.js changing the const ENDPIONTurl.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://react-application-chat.herokuapp.com/';
+  const ENDPOINT = 'http://localhost:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
