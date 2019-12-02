6809e954b130cfaa2886fac8729be02f7e3e7948
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-12-02 14:25:44 +0100

    fortfarande inte kan skicka image

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -6,11 +6,8 @@ import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
-
 import './Chat.css';
-
 let socket;
-
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
@@ -18,53 +15,35 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'http://localhost:5000';
-
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
-
     socket = io(ENDPOINT);
-
     setRoom(room);
     setName(name)
-
     socket.emit('join', { name, room }, (error) => {
       if(error) {
         alert(error);
       }
     });
   }, [ENDPOINT, location.search]);
-
   useEffect(() => {
     socket.on('message', (message) => {
       setMessages([...messages, message ]);
     });
-
     socket.on('roomData', ({ users }) => {
       setUsers(users);
     })
-
     return () => {
       socket.emit('disconnect');
-
       socket.off();
     }
   }, [messages])
-
   const sendMessage = (event) => {
     event.preventDefault();
-    /* if (message === "/randompicture") {
-      url ='https://randomuser.me/api/?results=1'
-      const response = await fetch(url);
-      const data = await response.json();
-      const  large  = data.results[0].picture.large;
-      message =  '<img src="'+large+'">';     
-    } */
-
     if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
-
   return (
     <div className="outerContainer">
       <div className="container">
@@ -76,5 +55,4 @@ const Chat = ({ location }) => {
     </div>
   );
 }
-
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,59 +2,55 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
-
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
 const router = require('./router');
-
+const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
 app.use(cors());
 app.use(router);
-
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
+   /*  var readStream = fs.createReadStream(path.resolve(__dirname,'./dance.jpg'),
+    {encoding:'binary'}),chunks = [];
+    readStream.on('readable',function(){
+      console.log('Image loading ...')
+    });
+    readStream.on('data',function(chunk){
+      chunks.push(chunk);
+      socket.emit('img-chunk',chunk);
+    });
+    readStream.on('end',function(){
+      console.log('Image loaded');
+    }); */
     const { error, user } = addUser({ id: socket.id, name, room });
-
     if(error) return callback(error);
-
     socket.join(user.room);
-
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
     callback();
   });
-
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-
-    // Read message 
+    // Read message
     // Check if message is "/dance"
-    // 
+    //
     if(message === "/danse"){
-      io.to(user.room).emit('message', { user: user.name, text:"is dansening"});
+      io.to(user.room).emit('message', { user: user.name, text:image});
     }
     else{
-      
       io.to(user.room).emit('message', { user: user.name, text: message });
     }
-
     callback();
   });
-
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
-
     if(user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     }
   })
 });
-
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
